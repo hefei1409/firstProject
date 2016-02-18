883892ada09644573074d05957b3edcce0650a8b
hefei1409 <1484260472@qq.com>
2016-02-18 22:24:53 +0800

    he

---
+++ 002/002/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  002
+//
+//  Created by qianfeng on 16/2/18.
+//  Copyright (c) 2016年 hf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
